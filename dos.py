251cdbd9d076f7c424a1790106f5a442fb3f9874
Ryan Propper <rpropper@cs.stanford.edu>
2017-05-09 17:54:56 +0000

    Run attacker in separate thread

--- dos.py
+++ dos.py
@@ -2,16 +2,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import print_function
+
 import os
 import time
 import subprocess
+import threading
+
 import mininet.topo
 import mininet.net
 import mininet.node
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="TCP DoS simulator.")
+
+parser.add_argument('--burst', '-b',
+                    help="Burst duration in seconds of each DoS attack.",
+                    type=float,
+                    default=0.15)
+
+parser.add_argument('--cong',
+                    help="Congestion control algorithm to use.",
+                    default="reno")
+
+parser.add_argument('--period', '-p',
+                    help="Seconds between low-rate DoS attacks, e.g. 0.5",
+                    type=float,
+                    default=0.5)
+args = parser.parse_args()
 
 class Topo(mininet.topo.Topo):
     def build(self):
+        # Topology:
+        #  A (alice) ------+
+        #                  |--[s0]----[s1]----server
+        #  M (mallory) ----+
         alice = self.addHost('alice')
         mallory = self.addHost('mallory')
         local_switch = self.addSwitch('s0')
@@ -41,6 +66,21 @@ def stop_tcpprobe(p):
     p.terminate()
 
 
+# "In these experiments, we again consider the scenario of Figure 2 but
+# with a single TCP flow.4 The TCP Reno flow has minRTO = 1 second and
+# satisfies conditions (C1) and (C2)."
+def set_rto_min(net):
+    # From: https://serverfault.com/questions/529347/how-to-apply-rto-min-to-a-certain-host-in-centos
+    alice = net.get('alice')
+    current_config = alice.cmd('ip route show').strip()
+    print('Initial ip route config: %s' % current_config)
+
+    new_config = '%s rto_min 1s' % current_config
+    print('Setting new config for alice (%s): %s' % (
+        alice.IP(), new_config))
+    alice.cmd('sudo ip route change %s' % new_config, shell=True)
+
+
 def start_iperf(net):
     alice = net.get('alice')
     server = net.get('server')
@@ -48,34 +88,51 @@ def start_iperf(net):
     iperf_s = alice.popen('iperf -s > server', shell=True)
     print('Starting iperf client on {}.'.format(server.IP()))
     iperf_c = server.popen(
-        'iperf -c {} -t {} > client'.format(alice.IP(), 600), shell=True)
+        'iperf -c {} -t {} > client'.format(alice.IP(), 60), shell=True)
     print('Iperf started on server and client.')
     return (iperf_c, iperf_s)
 
 
+def start_attacker(net, period, burst_length):
+    mallory = net.get('mallory')
+    server = net.get('server')
+
+    print('Starting ICMP flood: %s -> %s' % (mallory.IP(), server.IP()))
+    while True:
+      print('  ** ping burst!')
+      p = mallory.popen('ping -f {}'.format(server.IP()))
+      time.sleep(burst_length)
+      p.terminate()
+
+      # Now wait for the period-between-bursts to do the square wave attack
+      time.sleep(period)
+
+
 def main():
     subprocess.call(
-        'sysctl -q -w net.ipv4.tcp_congestion_control=reno', shell=True)
+        'sysctl -q -w net.ipv4.tcp_congestion_control=%s' % args.cong,
+        shell=True)
+
     topo = Topo()
     net = mininet.net.Mininet(
         topo=topo, host=mininet.node.CPULimitedHost, link=mininet.link.TCLink)
     net.start()
     net.pingAll()
+
+    set_rto_min(net)
+
     client, server = start_iperf(net)
     probe = start_tcpprobe()
-    for _ in range(60):
-        time.sleep(10)
-        mallory = net.get('mallory')
-        server = net.get('server')
 
-        print('Starting ICMP flood: %s -> %s' % (mallory.IP(), server.IP()))
-        p = mallory.popen('ping -f {}'.format(server.IP()))
+    attack_thread = threading.Thread(target=start_attacker,
+                                     args=(net, args.period, args.burst))
+    attack_thread.daemon = True
+    attack_thread.start()
 
-        time.sleep(2)
-        p.terminate()
+    client.communicate()
+
     stop_tcpprobe(probe)
     print('Cleaning up processes.')
-    client.terminate()
     server.terminate()
     net.stop()
 
