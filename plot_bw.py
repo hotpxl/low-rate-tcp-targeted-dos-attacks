9f836540ee84adf521acccd9370eb580438fc08e
Yutian Li <hotpxless@gmail.com>
2017-05-13 07:38:47 +0000

    Update plot.

--- plot_bw.py
+++ plot_bw.py
@@ -12,34 +12,23 @@ import numpy as np
 
 
 def main():
-    plt.figure(figsize=(20, 40))
     files = [i for i in os.listdir('.') if i.startswith('tx-')]
     if len(files) == 0:
         print('No input files.')
         sys.exit(1)
-    rows = int(math.sqrt(len(files)))
-    cols = math.ceil(len(files) / rows)
-    for idx, f in enumerate(files):
+    bursts = {}
+    for f in files:
         period, burst = tuple(map(float, f[3:-4].split('-')))
+        bursts.setdefault(burst, [])
         with open(f) as f:
-            # Skip last line because it might be incomplete.
-            tss = []
-            out_rates = []
-            start_ts = None
-            for l in f.readlines()[:-1]:
-                ts, interface, out_rate, in_rate, total_rate = l.split(',')[:5]
-                if interface == 'alice-eth0':
-                    if start_ts is None:
-                        start_ts = int(ts)
-                    tss.append(int(ts) - start_ts)
-                    out_rates.append(min(float(out_rate), 5e6))
-        plt.subplot(rows, cols, idx + 1)
-        plt.plot(
-            tss,
-            out_rates,
-            label='period: {}, burst: {}'.format(period, burst),
-            lw=1)
-        plt.legend()
+            # Transferred 80 Mbits.
+            t = 80 / float(f.readline().strip())
+            bursts[burst].append((period, t))
+    for burst in bursts:
+        plt.plot(*zip(*sorted(bursts[burst])), label='burst: {}'.format(burst))
+    plt.xlabel('Period (s)')
+    plt.ylabel('Average rate (Mbits/s)')
+    plt.legend()
     plt.savefig('tx.png', bbox_inches='tight')
 
 
