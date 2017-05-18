ea4d09f6e07a6a9522fcec39f0361f921a96a379
Yutian Li <hotpxless@gmail.com>
2017-05-18 07:24:06 +0000

    Test with all bursts.

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ if [[ "${UID}" -ne 0 ]]; then
   exit 1
 fi
 
-for period in $(seq 1.6 0.1 2.0); do
+for period in $(seq 0.5 0.1 2.0); do
+  for burst in $(seq 0.05 0.05 0.2); do
     mn -c 2> /dev/null
     killall -9 python dd nc tshark dumpcap || true
-    python dos.py --rto=1000 --period "${period}" --burst 0.1
+    python dos.py --rto=1000 --period "${period}" --burst "${burst}"
     killall -9 python dd nc tshark dumpcap || true
+  done
 done
