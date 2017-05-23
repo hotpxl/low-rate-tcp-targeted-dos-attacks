5c6a9e288ad7030f2c6f1997c025c0cccb1ba00c
Ryan Propper <rpropper@cs.stanford.edu>
2017-05-23 04:05:10 +0000

    Add "basic" and "full" run modes -- basic is faster, full tests more burst and period combinations

--- run.sh
+++ run.sh
@@ -6,12 +6,28 @@ if [[ "${UID}" -ne 0 ]]; then
   exit 1
 fi
 
+# "full" mode runs more period and burst combinations.
+full=false
+while getopts 'abfv' flag; do
+  case "${flag}" in
+    f) full=true ;;
+    *) error "Use either 'sudo ./run.sh' or 'sudo ./run.sh -f'." ;;
+  esac
+done
+
+max_burst=0.15
+max_period=1.5
+if [ "$full" = true ]; then
+  max_burst=0.25
+  max_period=2.0
+fi
+
 START_TIME=`date +%Y%m%d-%H%M%S`
 HOSTNAME=`hostname`
 echo "Run $START_TIME on $HOSTNAME started."
 
-for burst in $(seq 0.15 0.05 0.25); do
-  for period in $(seq 0.5 0.1 2.0); do
+for burst in $(seq 0.15 0.05 ${max_burst}); do
+  for period in $(seq 0.5 0.05 ${max_period}); do
     mn -c 2> /dev/null
     killall -9 python dd nc tshark dumpcap || true
 
